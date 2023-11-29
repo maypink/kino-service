95b058c18d4ea91e80c57d3a32cae2db1fc870a6
Pinchuk Maya <mayaprr@yandex.ru>
2023-11-30 00:44:02 +0300

    setup gets in controller

--- app/src/main/java/kino/films/controller/FilmController.java
+++ app/src/main/java/kino/films/controller/FilmController.java
@@ -2,8 +2,6 @@ package kino.films.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
 import kino.films.exception.customException.FilmNotFoundException;
 import kino.films.service.FilmService;
 import kino.films.utils.FilmResource;
@@ -28,10 +26,10 @@ public class FilmController {
     FilmService filmService;
 
     @Operation(
-            summary = "Get film from database."
+            summary = "Get films from database by title."
     )
     @GetMapping("/{title}")
-    public ResponseEntity<?> get(@PathVariable @Parameter(description = "title") String title) {
+    public ResponseEntity<?> getFilmsByTitle(@PathVariable @Parameter(description = "title") String title) {
 
         List<FilmResource> filmResources = filmService.getFilmsByTitle(title);
         if (filmResources.isEmpty()){
@@ -39,5 +37,42 @@ public class FilmController {
         } else {
             return ResponseEntity.status(HttpStatus.OK).body(filmResources);
         }
+    }
+
+    @Operation(
+            summary = "Get films from database by genre."
+    )
+    @GetMapping("/{genre}")
+    public ResponseEntity<?> getFilmsByGenre(@PathVariable @Parameter(description = "genre") String genre) {
+
+        List<FilmResource> filmResources = filmService.getFilmsByGenre(genre);
+        if (filmResources.isEmpty()){
+            throw new FilmNotFoundException("Film with specified genre was not found.");
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(filmResources);
+        }
+    }
+
+    @Operation(
+            summary = "Get films from database by year."
+    )
+    @GetMapping("/{year}")
+    public ResponseEntity<?> getFilmsByYear(@PathVariable @Parameter(description = "year") Integer year) {
+
+        List<FilmResource> filmResources = filmService.getFilmByYear(year);
+        if (filmResources.isEmpty()){
+            throw new FilmNotFoundException("Film with specified year was not found.");
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(filmResources);
+        }
+    }
+
+    @Operation(
+            summary = "Get all films from database."
+    )
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllFilms() {
+        List<FilmResource> weatherResources = filmService.getAllFilms();
+        return ResponseEntity.status(HttpStatus.OK).body(weatherResources);
     }
 }
